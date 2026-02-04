0e43d1981a1f0e15f96f74d861e1051a924228a7
Jake Chung <135403781+boosiie@users.noreply.github.com>
2026-02-04 06:34:34 +0000

    good

---
+++ frontend/src/AboutPage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Filler words filler words filler words</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="title">Home Page. This is where everything will go</div>
+  );
+}
+
+export default HomePage;
